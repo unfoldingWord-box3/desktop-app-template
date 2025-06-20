9037d10dfba3b4bacb3b8229f5671656fab7c80f
RUN-Projects <82665975+RUN-Projects@users.noreply.github.com>
2025-06-20 16:10:34 -0500

    Ask if server is running

--- macos/scripts/bundle_zip.zsh
+++ macos/scripts/bundle_zip.zsh
@@ -4,62 +4,74 @@
 
 # run from pankosmia\desktop-app-[APP NAME]/macos/scripts directory in powershell by:  ./bundle_tgz.bsh
 
-source ../../app_config.env
+echo
+if read -q "choice?Is the server off?[Y/N]? "; then
 
-if [ ! -f ../../local_server/target/release/local_server ]; then
+  source ../../app_config.env
+
+  if [ ! -f ../../local_server/target/release/local_server ]; then
+    echo
+    echo "   ***************************************************************"
+    echo "   * IMPORTANT: Build the local server, then re-run this script! *"
+    echo "   ***************************************************************"
+    echo
+    pause
+    exit
+  fi
+
   echo
-  echo "   ***************************************************************"
-  echo "   * IMPORTANT: Build the local server, then re-run this script! *"
-  echo "   ***************************************************************"
+  echo "Running app_setup to ensure version number consistency between buildSpec.json and this build bundle:"
+  ./app_setup.bsh
+
   echo
-  pause
-  exit
+  echo "Version is $APP_VERSION"
+  echo
+
+  cd ../../
+
+  if [ $(ls releases/macos/*.zip 2>/dev/null | wc -l) -gt 0 ]; then
+    echo "A previous macos .zip release exists. Removing..."
+    rm releases/macos/*.zip
+  fi
+
+  echo "checkout main"
+  git checkout main &> /dev/null
+  echo "pull"
+  git pull
+  echo "npm install"
+  npm install
+  cd macos/scripts
+
+  if [ -f ../build ]; then
+    echo "Removing last build environment"
+    rm -rf ../build
+  fi
+
+  echo "Assembling build environment"
+  node build.js
+  cd ../build
+  echo
+  echo "   **********************************"
+  echo "   *                                *"
+  echo "   *           ====                 *"
+  echo "   * Bundling. Wait for the prompt. *"
+  echo "   *           ====                 *"
+  echo "   *                                *"
+  echo "   **********************************"
+  echo
+
+  cd ../build
+  # Use lower case app name in filename
+  APP_NAME=${APP_NAME:l}
+  chmod +x $APP_NAME.zsh
+  zip -r ../../releases/macos/$APP_NAME-macos-$APP_VERSION.zip * &> /dev/null
+  cd ../scripts
+
+else
+  echo
+  echo
+  echo "Exiting..."
+  echo
+  echo "If the server is on, turn it off with Ctrl-C in the terminal window in which it is running, then re-run this script.";
+  echo
 fi
-
-echo
-echo "Running app_setup to ensure version number consistency between buildSpec.json and this build bundle:"
-./app_setup.bsh
-
-echo
-echo "Version is $APP_VERSION"
-echo
-
-cd ../../
-
-if [ $(ls releases/macos/*.zip 2>/dev/null | wc -l) -gt 0 ]; then
-  echo "A previous macos .zip release exists. Removing..."
-  rm releases/macos/*.zip
-fi
-
-echo "checkout main"
-git checkout main &> /dev/null
-echo "pull"
-git pull
-echo "npm install"
-npm install
-cd macos/scripts
-
-if [ -f ../build ]; then
-  echo "Removing last build environment"
-  rm -rf ../build
-fi
-
-echo "Assembling build environment"
-node build.js
-cd ../build
-echo
-echo "   **********************************"
-echo "   *                                *"
-echo "   *           ====                 *"
-echo "   * Bundling. Wait for the prompt. *"
-echo "   *           ====                 *"
-echo "   *                                *"
-echo "   **********************************"
-echo
-
-cd ../build
-# Use lower case app name in filename
-APP_NAME=${APP_NAME:l}
-chmod +x $APP_NAME.zsh
-zip -r ../../releases/macos/$APP_NAME-macos-$APP_VERSION.zip * &> /dev/null
-cd ../scripts
--- macos/scripts/run.zsh
+++ macos/scripts/run.zsh
@@ -3,25 +3,36 @@
 set -e
 set -u
 
-# set port environment variable
-export ROCKET_PORT=19119
+echo
+if read -q "choice?Only one instance of the server can be running at a time. Is the server off?[Y/N]? "; then
 
-if [ -d ../build ]; then
-  echo "Removing last build environment"
-  rm -rf ../build
+  # set port environment variable
+  export ROCKET_PORT=19119
+
+  if [ -d ../build ]; then
+    echo "Removing last build environment"
+    rm -rf ../build
+  fi
+
+  if [ ! -d ../build ]; then
+    echo "Assembling build environment"
+    node ./build.js
+  fi
+
+  echo "Running..."
+  cd ../build
+
+  SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+  export APP_RESOURCES_DIR="$SCRIPT_DIR/lib/"
+
+  SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+  $SCRIPT_DIR/bin/server.bin
+
+else
+  echo
+  echo
+  echo "Exiting..."
+  echo
+  echo "If the server is on, turn it off with Ctrl-C in the terminal window in which it is running, then re-run this script.";
+  echo
 fi
-
-if [ ! -d ../build ]; then
-  echo "Assembling build environment"
-  node ./build.js
-fi
-
-echo "Running..."
-cd ../build
-
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-export APP_RESOURCES_DIR="$SCRIPT_DIR/lib/"
-
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-$SCRIPT_DIR/bin/server.bin
-
