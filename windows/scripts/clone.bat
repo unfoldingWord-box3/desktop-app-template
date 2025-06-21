0d6a6592341b7326a830b47fc2c830ae310b5d02
RUN-Projects <82665975+RUN-Projects@users.noreply.github.com>
2025-06-20 23:21:35 -0500

    Strip single apostrophes from APP_NAME and change spaces to dashes

--- windows/scripts/clone.bat
+++ windows/scripts/clone.bat
@@ -37,6 +37,12 @@ for /l %%a in (1,1,%count%) do (
     echo.
   )
 )
+
+REM Remove apostrophes (') from APP_NAME for file path
+set APP_NAME=%APP_NAME:'=%
+REM Change spaces to dashes (-) from APP_NAME for file path
+set APP_NAME=%APP_NAME: =-%
+REM Ignoring uppercase in APP_NAME as they are treated the same as lowercase in windows file paths
 cd desktop-app-%APP_NAME%\windows\scripts
 
 endlocal
